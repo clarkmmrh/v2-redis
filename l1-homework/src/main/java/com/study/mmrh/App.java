0ddbf39f3f5bb5a54d6e5b8740898eefde61e56a
clarkmmrh <clarkmmrh@163.com>
2020-11-16 23:21:28 +0800

    step3 test

---
+++ l1-homework/src/main/java/com/study/mmrh/App.java
@@ -0,0 +1,14 @@
+package com.study.mmrh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("git test");
+    }
+}
