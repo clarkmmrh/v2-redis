0d55b1e64da6b68b5d256adab3e3f5efd233c86f
clarkmmrh <clarkmmrh@163.com>
2020-11-17 00:59:23 +0800

    idea commit

--- l1-homework/src/main/java/com/study/mmrh/App.java
+++ l1-homework/src/main/java/com/study/mmrh/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("git test 8 in idea");
+        System.out.println("git test 9 in idea and push");
     }
 }
