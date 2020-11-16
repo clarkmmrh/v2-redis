83a9baafda594390dae5dfa40c9ea373c9db54e8
clarkmmrh <clarkmmrh@163.com>
2020-11-17 00:51:39 +0800

    step7 test

--- l1-homework/src/main/java/com/study/mmrh/App.java
+++ l1-homework/src/main/java/com/study/mmrh/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("git test 5");
+        System.out.println("git test 7");
     }
 }
